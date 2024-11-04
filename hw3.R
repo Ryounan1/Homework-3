d121c0500d3a30ef4250957f02ee30311e004ab4
Tenzin <tybapu@neiu.edu>
2024-11-04 23:05:48 +0000

    Update README.md with project details and instructions

--- hw3.R
+++ hw3.R
@@ -5,3 +5,14 @@
 # It is designed to help us incorporate Git Hub's features to share, collaborate, and
 #manage changes in an RStudio Project.
 
+### Assigned Roles
+## Member 1: Visualization 
+## Task: Creates stratified boxplot of text messages by Group.
+
+## Member 2**: Visualization #2
+## Task**: Creates stratified bar charts of text messages by Time.
+
+## Member 3**: Summary Statistics
+## Task**: Creates summary statistics of Text Messages by Group and Time.
+
+
