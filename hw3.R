ad1f4ef5d9c0db684cea14daf34f220dc4fb4a97
Tenzin <tybapu@neiu.edu>
2024-11-04 22:11:16 +0000

    Update with activity details and instructions.

---
+++ hw3.R
@@ -0,0 +1,7 @@
+#HW3 
+
+## Activity Details
+# In this project our group has to analyze a data set in RStudio and GitHub
+# It is designed to help us incorporate Git Hub's features to share, collaborate, and
+#manage changes in an RStudio Project.
+
